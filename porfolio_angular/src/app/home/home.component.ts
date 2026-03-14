b1bf047602a4d628cd14f0c79ae1445f339bf0fd
Fanomezantsoa Angela <fanomezantsoavonjiniainaangela@gmail.com>
2026-03-14 11:46:15 +0100

    modified:   porfolio_angular/src/app/home/home.component.html 	modified:   porfolio_angular/src/app/home/home.component.ts

--- porfolio_angular/src/app/home/home.component.ts
+++ porfolio_angular/src/app/home/home.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { HousingLocationInfo } from '../housinglocation';
+
 @Component({
    standalone: true,
-   imports: [HousingLocationComponent],
+   imports: [],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: HousingLocationInfo = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+
 
 }
