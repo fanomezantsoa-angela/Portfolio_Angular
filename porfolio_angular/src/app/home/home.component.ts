051db1c11946490848eaf1578d360a3e8f267462
Fanomezantsoa Angela <fanomezantsoavonjiniainaangela@gmail.com>
2026-03-18 22:16:11 +0100

    contact icon

--- porfolio_angular/src/app/home/home.component.ts
+++ porfolio_angular/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { PresentationComponent } from '../presentation/presentation.component';
+import { ContactsComponent } from '../contacts/contacts.component';
 @Component({
    standalone: true,
-   imports: [PresentationComponent],
+   imports: [PresentationComponent, ContactsComponent],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
